dd0c0780ca3effa76f66c2ace7fa4614ee649eb4
Patrick Taburet <taburet.patrick@gmail.com>
2024-06-24 23:52:07 +0200

    Starting data manipulation.

--- sketch.js
+++ sketch.js
@@ -1,22 +1,43 @@
 
-let USdata;
-let CHIdata;
-let INdata;
+let USrowData;
+let CHIrowData;
+let INrowData;
+let totalPopulation;
+let colors;
+let areas = {};
 
 function preload() {
-    USdata = loadJSON( "/Us_pop.txt");
-    CHIdata = loadJSON( "/India_pop.txt");
-    INdata = loadJSON( "/China_pop.txt");
+    USrowData = loadJSON( "/Us_pop.txt");
+    CHIrowData = loadJSON( "/India_pop.txt");
+    INrowData = loadJSON( "/China_pop.txt");
 }
 
 function setup(){
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(700, 700);
     colorMode(HSB);
     background(0,0,0);
-    console.log(extractData(USdata));
-    console.log(extractData(CHIdata));
-    console.log(extractData(INdata));
-   
+    noStroke();
+    let data = {
+        usa: extractData(USrowData),
+        china: extractData(CHIrowData),
+        india: extractData(INrowData)
+    };
+    console.log(data.china);
+
+    colors = {
+        china: [255, 0, 0],
+        india: [0, 255, 0],
+        usa: [0, 0, 255]
+    };
+    let CHIMaxPop = data.china[data.china.length - 1];
+    let USAMaxPop = data.usa[data.usa.length - 1];
+    let INDiMaxPop = data.india[data.india.length - 1];
+    totalPopulation = CHIMaxPop + USAMaxPop + INDiMaxPop
+    console.log(totalPopulation);
+
+
+    let totalArea = width * height;
+  
 }
 
 function draw(){
@@ -30,7 +51,7 @@ function extractData(country){
 }
 
 function windowResized(){
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(700, 700);
     colorMode(HSB);
     background(0,0,0);
 }
