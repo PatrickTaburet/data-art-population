7efb0fc232a2c17c0bc1edbdd7903471cbba5445
Patrick Taburet <taburet.patrick@gmail.com>
2024-07-02 23:57:28 +0200

    Architecture restructuration.

--- sketch.js
+++ sketch.js
@@ -1,147 +1,139 @@
-
 let USrowData;
 let CHIrowData;
 let INrowData;
-let totalPopulation;
+
 let colors = {
     china: 255,
     india: 150,
     usa: 50
 };
-let areas = {};
-let data={};
-let USAMaxPop;
-let CHIMaxPop;
-let INDiMaxPop;
-let origin= {};
+let origin = {};
 
 function preload() {
-    USrowData = loadJSON( "/Us_pop.txt");
-    CHIrowData = loadJSON( "/India_pop.txt");
-    INrowData = loadJSON( "/China_pop.txt");
+    USrowData = loadJSON("/Us_pop.txt");
+    CHIrowData = loadJSON("/India_pop.txt");
+    INrowData = loadJSON("/China_pop.txt");
 }
 
-function setup(){
+function setup() {
     createCanvas(1200, 1200);
     colorMode(HSB);
-    background(0,0,0);
+    background(0, 0, 0);
     noStroke();
+    frameRate(30);
     
-    frameRate(30)
-    let totalArea = width * height;
     origin = {
         usa: width / 2,
         china: width / 2,
         india: width / 2
-      };
-    data = {
-        usa: extractData(USrowData),
-        china: extractData(CHIrowData),
-        india: extractData(INrowData)
     };
-    // console.log(data.china);
 
- 
+    let dataManager = new DataManager(USrowData, CHIrowData, INrowData);
+    dataManager.calculateAreas();
 
-    USAMaxPop = data.usa[data.usa.length - 1];
-    CHIMaxPop = data.china[data.china.length - 1];
-    INDiMaxPop = data.india[data.india.length - 1];
+    let usa = new Country('usa', dataManager);
+    let china = new Country('china', dataManager);
+    let india = new Country('india', dataManager);
 
-    totalPopulation = CHIMaxPop + USAMaxPop + INDiMaxPop
-
-    areas.usa = (USAMaxPop / totalPopulation) * totalArea;
-    areas.china = (CHIMaxPop / totalPopulation) * totalArea;
-    areas.india = (INDiMaxPop / totalPopulation) * totalArea;
-    // console.log(areas);
-    // console.log(totalArea);
-    ellipseMode("center");
-    
-    // Doubler tableaux data
-    data = {
-        usa: doubleTable(data.usa),
-        china: doubleTable(data.china),
-        india: doubleTable(data.india)
-    };
-   
+    window.countries = [usa, china, india];
 }
 
-function draw(){ 
-    
+function draw() {
     background(0, 0, 0);
-
-     // Country-specific variables
-
-    drawData("usa");
-    drawData("china");
-    drawData("india");
- 
- 
-     
-    // Generate a random shape using Perlin noise
-
-   
-   
-    
-    // let index = frameCount % data.usa.length;
-    // console.log(index);
-    // let element = data.usa[index]
-    // let proportion = element / USAMaxPop;
-    // let size = sqrt(proportion * areas.usa)
-    // ellipse(index+width/2, index+width/2, size, size)
-
-    // fill(colors.china, 100,100,0.5);
-    // let index2 = frameCount % data.china.length;
-    // console.log(index2);
-    // let element2 = data.china[index2]
-    // let proportion2= element2 / CHIMaxPop;
-    // let size2 = sqrt(proportion2 * areas.china)
-    // rect(index2, index2, size2, size2)
-
+    countries.forEach(country => country.drawData());
 }
 
-function extractData(country){
-    let countryData = country[1].map(item => item.value);
-    cleanData = countryData.reverse().slice(0, -1);
-    return cleanData;
-}
-
-function windowResized(){
+function windowResized() {
     createCanvas(1200, 1200);
     colorMode(HSB);
-    background(0,0,0);
-}
-function calculCountryParams(country){
-    let index = (frameCount % data[country].length);
-    let maxPop = data[country][data[country].length - 1];
-    let area = areas[country];
-    let proportion = data[country][index] / maxPop;
-    let size = sqrt(proportion * area);
-    return params ={
-        index: index,
-        maxPop: maxPop,
-        size : size
-    }
+    background(0, 0, 0);
 }
 
-function drawData(country){
-    params = calculCountryParams(country);
-    fill(colors[country], 100,100, 0.5);
-    // console.log(params.index);
-    ellipse((params.index+origin[country]), params.index+origin[country], params.size, params.size)
-}
 function mouseClicked() {
     origin.usa = random(width);
     origin.china = random(width);
     origin.india = random(width);
-  }
-function doubleTable(table) {
-    let result = [table[0]];
-    for (let i = 0; i < table.length - 1; i++) {
-      let avg = (table[i] + table[i + 1]) / 2;
-      result.push(avg, table[i + 1]);
+}
+
+class DataManager {
+    constructor(USData, CHIData, INData) {
+        this.USData = this.extractData(USData);
+        this.CHIData = this.extractData(CHIData);
+        this.INData = this.extractData(INData);
+        
+        this.data = {
+            usa: this.doubleTable(this.USData),
+            china: this.doubleTable(this.CHIData),
+            india: this.doubleTable(this.INData)
+        };
+        
+        this.USAMaxPop = this.USData[this.USData.length - 1];
+        this.CHIMaxPop = this.CHIData[this.CHIData.length - 1];
+        this.INDiMaxPop = this.INData[this.INData.length - 1];
+        
+        this.totalPopulation = this.CHIMaxPop + this.USAMaxPop + this.INDiMaxPop;
+        this.totalArea = width * height;
+        
+        this.areas = {};
     }
-    return result;
-  }
+    
+    extractData(countryData) {
+        let country = countryData[1].map(item => item.value);
+        return country.reverse().slice(0, -1);
+    }
+    
+    doubleTable(table) {
+        let result = [table[0]];
+        for (let i = 0; i < table.length - 1; i++) {
+            let avg = (table[i] + table[i + 1]) / 2;
+            result.push(avg, table[i + 1]);
+        }
+        return result;
+    }
+    
+    calculateAreas() {
+        this.areas.usa = (this.USAMaxPop / this.totalPopulation) * this.totalArea;
+        this.areas.china = (this.CHIMaxPop / this.totalPopulation) * this.totalArea;
+        this.areas.india = (this.INDiMaxPop / this.totalPopulation) * this.totalArea;
+    }
+}
+
+class Country {
+    constructor(name, dataManager) {
+        this.name = name;
+        this.dataManager = dataManager;
+        this.data = dataManager.data[name];
+        this.maxPop = this.data[this.data.length - 1];
+        this.area = dataManager.areas[name];
+        this.index = 0;
+    }
+
+    calculateParams() {
+        this.index = frameCount % this.data.length;
+        let proportion = this.data[this.index] / this.maxPop;
+        let size = sqrt(proportion * this.area);
+        return { size: size, index: this.index };
+    }
+
+    drawData() {
+        let params = this.calculateParams();
+        fill(colors[this.name], 100, 100, 0.5);
+        let x = params.index + origin[this.name];
+        let y = params.index + origin[this.name];
+        let size = params.size;
+        if (x - size / 2 < 0) {
+            x = size / 2;
+        } else if (x + size / 2 > width) {
+            x = width - size / 2;
+        }
+        if (y - size / 2 < 0) {
+            y = size / 2;
+        } else if (y + size / 2 > height) {
+            y = height - size / 2;
+        }
+        ellipse(x, y, size, size);
+    }
+}
 
 
 // TEST PERLIN NOISE SHAPE
